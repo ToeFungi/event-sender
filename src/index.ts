fddd4b94891382b9b1b52c6862de202c08ea2245
Alex Pickering <pickeringacw@gmail.com>
2019-12-05 17:18:48 +0200

    Implement controller to orchestrate services, implement custom errors and handling

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 import * as AWS from 'aws-sdk'
+
 import { SchedulerRepository } from './repositories/SchedulerRepository'
 
 const ddb = new AWS.DynamoDB.DocumentClient({
   region: 'eu-west-1'
 })
 const schedulerRepository = new SchedulerRepository(ddb)
-
-schedulerRepository.getScheduledEvents('2019-10-04T15:43')
-  .then(console.log)
-  .catch(console.error)
